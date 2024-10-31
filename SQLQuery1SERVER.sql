3804a4138e75341a98e1f39e0bc99c3f6311fb1b
JuanesAS448 <159972584+JuanesAS448@users.noreply.github.com>
2024-10-31 09:34:59 -0500

    Update SQLQuery1SERVER.sql

--- SQLQuery1SERVER.sql
+++ SQLQuery1SERVER.sql
@@ -2,29 +2,29 @@ CREATE DATABASE LosDesterrados;
 USE LosDesterrados;
 
 CREATE TABLE Cultivo (
-    id_cultivo INT PRIMARY KEY,
-    tipo_planta VARCHAR(50),
+    id_cultivo INT PRIMARY KEY IDENTITY(1,1),
+    tipo_planta NVARCHAR(50) NOT NULL,
     fecha_siembra DATE,
-    estado_crecimiento VARCHAR(50),
+    estado_crecimiento NVARCHAR(50),
     rendimiento_esperado DECIMAL(10, 2)
 );
 
 CREATE TABLE Insumo (
-    id_insumo INT PRIMARY KEY,
-    tipo_insumo VARCHAR(50),
+    id_insumo INT PRIMARY KEY IDENTITY(1,1),
+    tipo_insumo NVARCHAR(50) NOT NULL,
     cantidad_disponible DECIMAL(10, 2),
     fecha_caducidad DATE
 );
 
 CREATE TABLE Cliente (
-    id_cliente INT PRIMARY KEY,
-    nombre VARCHAR(100),
-    correo VARCHAR(100),
-    telefono VARCHAR(15)
+    id_cliente INT PRIMARY KEY IDENTITY(1,1),
+    nombre NVARCHAR(100),
+    correo NVARCHAR(100),
+    telefono NVARCHAR(15)
 );
 
 CREATE TABLE Venta (
-    id_venta INT PRIMARY KEY,
+    id_venta INT PRIMARY KEY IDENTITY(1,1),
     cliente_id INT,
     fecha_venta DATETIME,
     total DECIMAL(10, 2),
@@ -32,7 +32,7 @@ CREATE TABLE Venta (
 );
 
 CREATE TABLE Detalle_Venta (
-    id_detalle INT PRIMARY KEY,
+    id_detalle INT PRIMARY KEY IDENTITY(1,1),
     venta_id INT,
     cultivo_id INT,
     cantidad DECIMAL(10, 2),
